38a1dd7fe182a1445e0a466fc0ad3fd19766ace4
LiangGuanyu <lianggy0719@126.com>
2015-08-18 15:01:17 +0800

    WEB-128 datatranfer works well

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-var dt = require('./implements/dataTrans');
+// Main function of this service
+function onStart() {
+  var dt = require('./implements/dataTrans');
+}
 
+// Do not modify codes below!!
+
+if(process.argv[2] == 'start') {
+  onStart();
+  // initialize some event handler
+  process.on('SIGTERM', function() {
+    console.log('SIGTERM recived');
+    process.exit(0);
+  }).on('SIGINT', function() {
+    console.log('SIGINT recived');
+    process.exit(0);
+  });
+} else {
+  var svcmgr = require('webde-rpc').defaultSvcMgr();
+  svcmgr.addService('nodejs.webde.datatransfer', {
+    path: __dirname,
+    args: ['start'],
+    remote: false
+  }, function(ret) {
+    if(ret.err) {
+      console.log(ret.err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+}
